084bb92703e09cd3b20a6dc7bfb8168ebbf0e585
Abir Ahammed <ahammed.abir47@gmail.com>
2025-12-21 13:18:19 -0600

    process env url added

--- src/api/core/OpenAPI.ts
+++ src/api/core/OpenAPI.ts
@@ -20,7 +20,8 @@ export type OpenAPIConfig = {
 };
 
 export const OpenAPI: OpenAPIConfig = {
-    BASE: 'http://localhost:8080',
+    BASE: process.env.API_BASE_URL || 'http://localhost:8080',
+
     VERSION: '0',
     WITH_CREDENTIALS: false,
     CREDENTIALS: 'include',
